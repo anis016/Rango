5c29ae79a1c43e7e0b1c00881a681c1ccc95e9a3
anis016 <anis.cuet016@gmail.com>
2017-03-06 03:06:00 +0100

    Added some comments

--- www/rango/urls.py
+++ www/rango/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index', views.index, name='index'),
     url(r'^about', views.about, name='about'),
+
+    # User of ?P<name>
+    # In django, named capturing groups are passed to your view as "KEYWORD ARGUMENTS."
+    # Unnamed capturing groups (just a parenthesis) are passed to your view as "ARGUMENTS".
+    # The ?P is a named capturing group, as opposed to an unnamed capturing group.
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category')
 ]
