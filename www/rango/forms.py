5a95595c126e7fffd4e948cd79724ad00f85de5a
anis016 <anis.cuet016@gmail.com>
2017-03-14 06:47:37 +0100

    Added Bootstrap to all the pages

--- www/rango/forms.py
+++ www/rango/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text='Please enter the category name: ')
+    name = forms.CharField(max_length=128, label="Enter Category Name", help_text='Please enter the category name: ')
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -17,8 +17,8 @@ class CategoryForm(forms.ModelForm):
         fields = ('name', ) # only include these fields
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text='Please enter the title of the page: ')
-    url = forms.URLField(max_length=200, help_text='Please enter the URL of the page: ')
+    title = forms.CharField(max_length=128, label="Enter Page Title", help_text='Please enter the Title of the page: ')
+    url = forms.URLField(max_length=200, label="Enter Page URL", help_text='Please enter the URL of the page: ')
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0) ## by default it is required field
 
     class Meta:
